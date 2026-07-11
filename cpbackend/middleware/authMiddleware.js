2af029d0d85f691a62c1ba1c4e9eb80e0329488e
Swara <swaranatekar546@gmail.com>
2026-07-11 12:09:28 +0530

    fixed authMiddleware

--- cpbackend/middleware/authMiddleware.js
+++ cpbackend/middleware/authMiddleware.js
@@ -1,18 +1,16 @@
 const jwt = require('jsonwebtoken')
-const decoded = jwt.verify(token, process.env.JWT_SECRET)
 
-const authMiddleware = (req,res,next)=>{
-    try{
+const authMiddleware = (req, res, next) => {
+    try {
         const token = req.headers.authorization?.split(" ")[1];
-        if(!token){
-            res.status(401).json({message:"No Toekn Provided"})
+        if (!token) {
+            return res.status(401).json({ message: "No Token Provided" });
         }
-        const decoded = jwt.verify(token,"SecretKey")
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
         req.userId = decoded.id;
         next();
-    }
-    catch(err){
-        res.status(401).json({message:"Invalid token"})
+    } catch(err) {
+        res.status(401).json({ message: "Invalid token" });
     }
 }
 
